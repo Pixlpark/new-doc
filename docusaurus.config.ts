d136dc3812f58675ce2ba710a49896c6e3ca506b
vakhitov <vakhitov@pixlpark.com>
2023-12-23 19:36:45 +0700

    deploy

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
   url: 'https://pixlpark.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/new-doc',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
@@ -57,7 +57,7 @@ const config: Config = {
     navbar: {
       logo: {
         alt: 'Pixlpark Documentation',
-        src: 'img/logo-pixlpark.svg',
+        src: 'new-doc/img/logo-pixlpark.svg',
       },
       items: [
         {
